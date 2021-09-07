6601ae5bf534411dce41d1249e9ea0ff7a8e6f5d
Subhro Mukherjee <msubhro.mukherjee@gmail.com>
2021-09-08 00:28:03 +0530

    2nd update

--- src/components/home/Welcome.jsx
+++ src/components/home/Welcome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Welcome() {
     return (
         <div className="home_welcome">
-            <h2>Welcome to 2PF Food Center</h2>
+            <h2>Welcome to Subhro Fast Meal</h2>
             <h3>Order Your Favorite Food Now</h3>
             <a href="#order_now"><button className="btn_1">Order Now</button></a>
         </div>
